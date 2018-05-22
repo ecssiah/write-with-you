1fc6a70e2c0590a33bc046b1144eb6a5146d1a24
ecssiah <ecssiah@gmail.com>
2018-05-21 20:37:12 -0500

    Add top navigation bar links

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   devise_for :users
 
-  root 'pages#home'
+  root to: 'pages#home'
 
   resources :stories do
     resources :snippets, only: [:show, :new, :create, :edit, :update, :destroy]
