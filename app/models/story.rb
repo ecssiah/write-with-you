fb1e60dbd749a52425cf702f9c93346136cce25b
ecssiah <ecssiah@gmail.com>
2018-05-22 18:23:18 -0500

    Clean up the display of story titles

--- app/models/story.rb
+++ app/models/story.rb
@@ -2,4 +2,14 @@ class Story < ApplicationRecord
   belongs_to :user
   has_many :contributions
   has_many :snippets
+
+  def display_title
+    display = self.title
+
+    if !self.subtitle.blank?
+      display += ": <em>#{self.subtitle}</em>"
+    end
+
+    display.html_safe
+  end
 end
