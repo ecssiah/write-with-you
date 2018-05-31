f7ff58f85d280608e9624231d56909b697d1cb69
ecssiah <ecssiah@gmail.com>
2018-05-31 00:43:50 -0500

    Change voting path and move SessionsController logic to private methods

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,37 +5,42 @@ class SessionsController < ApplicationController
 
   def create
     omni_info = request.env['omniauth.auth']
-    
-    if omni_info
-      user = User.find_by(email: omni_info['info']['email'])
-
-      if !user
-        flash[:error] = "Email does not match existing user"
-        redirect_to login_path
-      else
-        login(user)
-      end
-    else
-      user = User.find_by(email: params[:user][:email])
-
-      if params[:user][:email].blank? || params[:user][:password].blank?
-        flash[:error] = "Fields can't be left blank"
-        redirect_to login_path
-      elsif !user
-        flash[:error] = "Email does not match existing user"
-        redirect_to login_path
-      elsif !user.authenticate(params[:user][:password])
-        flash[:error] = "Password is not correct"
-        redirect_to login_path
-      else
-        login(user)
-      end
-    end
+    omni_info ? omni_auth(omni_info) : local_auth(params)
   end
 
   def destroy
     session.delete(:user_id)
     redirect_to root_path
+  end
+
+  private
+
+  def omni_auth(info)
+    user = User.find_by(email: info['info']['email'])
+
+    if !user
+      flash[:error] = "Email does not match existing user"
+      redirect_to login_path
+    else
+      login(user)
+    end
+  end
+
+  def local_auth(info)
+    user = User.find_by(email: info[:user][:email])
+
+    if info[:user][:email].blank? || info[:user][:password].blank?
+      flash[:error] = "Fields can't be left blank"
+      redirect_to login_path
+    elsif !user
+      flash[:error] = "Email does not match existing user"
+      redirect_to login_path
+    elsif !user.authenticate(info[:user][:password])
+      flash[:error] = "Password is not correct"
+      redirect_to login_path
+    else
+      login(user)
+    end
   end
 
 end
