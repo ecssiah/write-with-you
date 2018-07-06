372f5280703f812516499f27e8e549e28915acc3
ecssiah <ecssiah@gmail.com>
2018-07-05 21:04:59 -0500

    Change user require_login macro to only instead of except

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :require_login, except: [:index, :all, :info, :show, :new, :create] 
+  before_action :require_login, only: [:edit, :update] 
 
   def index
     render json: current_user.stories.order(rank: :desc), status: 200
